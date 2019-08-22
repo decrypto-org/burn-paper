2259f80759a78aa77ec97d35629f0b984cb4b4a9
Kostis Karantias <kkarantias@gmail.com>
2019-08-22 20:14:31 +0200

    add burn address generator

---
+++ experiment/scripts/gen-burn-address.py
@@ -0,0 +1,33 @@
+import hashlib
+
+def hash_by_algo(algo, b):
+    h = hashlib.new(algo)
+    h.update(b)
+    return h.digest()
+
+def sha256(b):
+    return hash_by_algo('sha256', b)
+
+def sha512(b):
+    return hash_by_algo('sha512', b)
+
+def ripemd160(b):
+    return hash_by_algo('ripemd160', b)
+
+def tag(b):
+    pkr = sha512(b)
+    pkh = ripemd160(sha256(b"\x04"+pkr))
+    pkh_ = pkh[:-1] + (pkh[-1]^1).to_bytes(1, byteorder='little')
+    return b"\x00" + pkh_
+
+def main(t):
+    if t[:2] == '0x':
+        t = t[2:]
+    print(tag(bytes.fromhex(t)).hex())
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('address', help='an ethereum address')
+    args = parser.parse_args()
+    main(args.address)
