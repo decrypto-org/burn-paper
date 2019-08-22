2c775d2a54ea3c48d0795d2ed747d31fffca80b2
Kostis Karantias <kkarantias@gmail.com>
2019-08-22 23:47:28 +0200

    Add BurnVerifier

---
+++ experiment/contracts/BurnVerifier.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+library BurnVerifier {
+    function genBurnPKH(bytes memory tag) internal pure returns (bytes20) {
+        bytes20 result = ripemd160(abi.encodePacked(sha256(abi.encodePacked(hex"04", tag))));
+        result ^= hex"0000000000000000000000000000000000000001";
+        return result;
+    }
+    function verifyBurn(bytes memory tag, bytes20 pkh) public pure returns (bool) {
+        return genBurnPKH(tag) == pkh;
+    }
+}
